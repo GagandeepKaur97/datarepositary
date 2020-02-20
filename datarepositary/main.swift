dcfaa3547a57dbfafeb80563986a2fdb79a8bd81
gagandeepkaur <sherry2312.gk@gmail.com>
2020-02-20 18:12:13 -0500

    Initial Commit

---
+++ datarepositary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  datarepositary
+//
+//  Created by MacStudent on 2020-02-20.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
